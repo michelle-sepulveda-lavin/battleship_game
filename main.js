abdeadb88ebf13e37d5e988158772ba5584f6395
michelle-sepulveda-lavin <mic.sepulveda@outlook.com>
2020-10-19 11:38:27 -0300

    modificaciones opciones juego

--- main.js
+++ main.js
@@ -8,7 +8,10 @@ const span = document.getElementsByClassName("cerrar")[0];
 const input = document.getElementById("coordenadas");
 const disparaMisil = document.getElementById("dispararMisil");
 const tableroEnemigo = document.querySelector(".enemigo");
-const instrucciones = document.querySelector(".instrucciones");
+const tableroJugador = document.querySelector(".jugador")
+const instrucciones = document.querySelector(".instrucciones")
+const instruccionesLanchas = document.getElementById("soloLanchas");
+const instruccionesBarcos = document.getElementById("todosLosBarcos")
 const puntaje = document.getElementById("puntaje");
 const eliminados = document.getElementById("eliminados");
 const perdidos = document.getElementById("perdidos");
@@ -16,9 +19,16 @@ const modalGanador = document.getElementById("modalGanador");
 const tituloGanador = document.getElementById("tituloGanador");
 const jugarDeNuevo = document.getElementById("jugarDeNuevo");
 const mensaje = document.getElementById("mensaje");
+const opcionesJuego = document.getElementById("opcionesJuego")
+const opcionLanchas = document.getElementById("opcionSoloLanchas");
+const opcionFullBarcos = document.getElementById("opcionFullBarcos");
+const detalleBarcos = document.getElementById("barcos");
+const horizontal = document.getElementById("horizontal");
+const vertical = document.getElementById("vertical");
 
 let banderaBarcosEnemigos = "";
 let banderaBarcosJugador = "";
+let modoJuego = "";
 let bandera = false;
 let perdido = false;
 let hundido = false;
@@ -27,44 +37,53 @@ const barcos = [];
 const barcosJugador = [];
 const barcosElegidosPc = [];
 const disparosRealizados = [];
+const verificadorPosiciones = [];
 let contadorBarcosHundidos = 0;
 let contadorBarcosPerdidos = 0;
+let contador = 0
 
 /* crea un array de barcos del enemigo */
 const barcosEnemigos = () => {
+    const verificador = []
     for (let i = 1; i <= 10; i) {
         const posicionY = Math.floor(Math.random() * 9) + 1;
         const posicionX = Math.floor(Math.random() * 9) + 1;
-        if (!barcos.includes(`${posicionX}x${posicionY}y`)) {
-            barcos.push(`${posicionX}x${posicionY}y`)
-            i++
+        const coordenada = `${posicionX}x${posicionY}y`
+        if (!verificador.includes(coordenada)) {
+            if (!barcos.includes(coordenada)) {
+                barcos.push(coordenada)
+                verificador.push(coordenada, `${posicionX}x${posicionY - 1}y`, `${posicionX + 1}x${posicionY - 1}y`, `${posicionX + 1}x${posicionY}y`, `${posicionX + 1}x${posicionY + 1}y`)
+                verificador.push(`${posicionX}x${posicionY + 1}y`, `${posicionX - 1}x${posicionY + 1}y`, `${posicionX - 1}x${posicionY}y`, `${posicionX - 1}x${posicionY - 1}y`)
+                i++
+            }
         }
     }
     console.log(barcos)
 };
-const buscaGanador = (contador) =>{
-    if(contador == 10){
-        if(contador == contadorBarcosHundidos){
+/* busca el ganador */
+const buscaGanador = (contador) => {
+    if (contador == 10) {
+        if (contador == contadorBarcosHundidos) {
             modalGanador.style.display = "block";
             tituloGanador.innerHTML = "Ganaste!"
-        }else if(contador == contadorBarcosPerdidos){
+        } else if (contador == contadorBarcosPerdidos) {
             modalGanador.style.display = "block";
             tituloGanador.innerHTML = "Perdiste :("
         }
     }
 };
 /* mensaje que muestra */
-const defineMensaje = (estado) =>{
-    if(disparosRealizados.length === 0){
+const defineMensaje = (estado) => {
+    if (disparosRealizados.length === 0) {
         mensaje.style.color = "red";
         mensaje.innerHTML = "Realiza tu primer disparo!!"
-    }else if(estado === "red"){
+    } else if (estado === "red") {
         mensaje.style.color = "red";
         mensaje.innerHTML = "Has perdido un barco!"
-    }else if(estado == "green"){
+    } else if (estado == "green") {
         mensaje.style.color = "green";
         mensaje.innerHTML = "Hundiste un barco!!"
-    }else{
+    } else {
         mensaje.style.color = "rgb(234, 234, 241)";
         mensaje.innerHTML = "d"
     }
@@ -150,9 +169,9 @@ const capturaValor = (e) => {
 const dispararAlBarco = () => {
     bandera = false
     const barcoHundido = document.getElementById(misil);
-    const patron =/[1-9][x][1-9][y]/i;
+    const patron = /[1-9][x][1-9][y]/i;
     const result = barcoHundido.id.match(patron);
-    if(result){
+    if (result) {
         if (!disparosRealizados.includes(barcoHundido)) {
             disparosRealizados.push(barcoHundido)
             if (barcos.includes(barcoHundido.id)) {
@@ -173,12 +192,84 @@ const dispararAlBarco = () => {
 };
 /* al iniciar, el jugador elige las posiciones de sus barcos */
 const pintaJugador = (e) => {
-    if (barcosJugador.length < 10) {
-        if (!barcosJugador.includes(e.target.id)) {
-            barcosJugador.push(e.target.id);
-            const posicion = document.getElementById(e.target.id);
-            posicion.style.border = "2px solid black"
+    if (modoJuego === "lanchas") {
+        if (barcosJugador.length < 10) {
+            const coordenada = e.target.id
+            const aux = coordenada.replace('x', '').replace('y', '');
+            if (verificadorPosiciones.length === 0) {
+                barcosJugador.push(e.target.id);
+                const posicion = document.getElementById(e.target.id);
+                posicion.style.border = "2px solid black"
+                verificadorPosiciones.push(coordenada, `${aux[0] - 1}y${aux[1]}x`, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${aux[1]}x`, `${aux[0]}y${aux[1] - 1}x`)
+                verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`, `${aux[0] - 1}y${parseInt(aux[1]) - 1}x`)
+            } else {
+                if (!verificadorPosiciones.includes(coordenada)) {
+                    verificadorPosiciones.push(coordenada, `${aux[0] - 1}y${aux[1]}x`, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${aux[1]}x`, `${aux[0]}y${aux[1] - 1}x`)
+                    verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`, `${aux[0] - 1}y${parseInt(aux[1]) - 1}x`)
+                    if (!barcosJugador.includes(e.target.id)) {
+                        barcosJugador.push(e.target.id);
+                        const posicion = document.getElementById(e.target.id);
+                        posicion.style.border = "2px solid black"
+                    }
+                }
+            }
         }
+    } else if (modoJuego === "tradicional") {
+        /* if (barcosJugador.length < 10) {
+            const coordenada = e.target.id
+            const aux = coordenada.replace('x', '').replace('y', '');
+            if (contador === 0) {
+                barcosJugador.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${aux[0]}y${parseInt(aux[1]) + 2}x`, `${aux[0]}y${parseInt(aux[1]) + 3}x`);
+                barcosJugador.forEach((item) => {
+                    const posicion = document.getElementById(item);
+                    posicion.style.border = "2px solid black"
+                })
+                contador += 1
+                verificadorPosiciones.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${aux[0]}y${parseInt(aux[1]) + 2}x`, `${aux[0]}y${parseInt(aux[1]) + 3}x`)
+                verificadorPosiciones.push(`${aux[0] - 1}y${aux[1]}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 1}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 2}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 3}x`)
+                verificadorPosiciones.push(`${parseInt(aux[0]) + 1}y${aux[1]}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 2}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 3}x`)
+                verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) - 1}x`, `${aux[0]}y${parseInt(aux[1]) - 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`)
+                verificadorPosiciones.push(`${parseInt(aux[0]) - 1}y${parseInt(aux[1]) + 4}x`, `${aux[0]}y${parseInt(aux[1]) + 4}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 4}x`)
+            } else if (contador === 1) {
+                if (!verificadorPosiciones.includes(coordenada)) {
+                    barcosJugador.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${aux[0]}y${parseInt(aux[1]) + 2}x`)
+                    barcosJugador.forEach((item) => {
+                        const posicion = document.getElementById(item);
+                        posicion.style.border = "2px solid black"
+                    })
+                    contador += 1
+                    verificadorPosiciones.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`, `${aux[0]}y${parseInt(aux[1]) + 2}x`)
+                    verificadorPosiciones.push(`${aux[0] - 1}y${aux[1]}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 1}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 2}x`)
+                    verificadorPosiciones.push(`${parseInt(aux[0]) + 1}y${aux[1]}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 2}x`)
+                    verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) - 1}x`, `${aux[0]}y${parseInt(aux[1]) - 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`)
+                    verificadorPosiciones.push(`${parseInt(aux[0]) - 1}y${parseInt(aux[1]) + 4}x`, `${aux[0]}y${parseInt(aux[1]) + 4}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 4}x`)
+                }
+            } else if (contador == 2) {
+                if (!verificadorPosiciones.includes(coordenada)) {
+                    barcosJugador.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`)
+                    barcosJugador.forEach((item) => {
+                        const posicion = document.getElementById(item);
+                        posicion.style.border = "2px solid black"
+                    })
+                    contador += 1
+                    verificadorPosiciones.push(coordenada, `${aux[0]}y${parseInt(aux[1]) + 1}x`)
+                    verificadorPosiciones.push(`${aux[0] - 1}y${aux[1]}x`, `${aux[0] - 1}y${parseInt(aux[1]) + 1}x`)
+                    verificadorPosiciones.push(`${parseInt(aux[0]) + 1}y${aux[1]}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 1}x`)
+                    verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) - 1}x`, `${aux[0]}y${parseInt(aux[1]) - 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`)
+                    verificadorPosiciones.push(`${parseInt(aux[0]) - 1}y${parseInt(aux[1]) + 4}x`, `${aux[0]}y${parseInt(aux[1]) + 4}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 4}x`)
+                }
+            } else if (contador == 3) {
+                if (!verificadorPosiciones.includes(coordenada)) {
+                    barcosJugador.push(coordenada)
+                    const posicion = document.getElementById(coordenada);
+                    posicion.style.border = "2px solid black"
+                    contador += 1
+                    verificadorPosiciones.push(coordenada, `${parseInt(aux[0]) + 1}y${aux[1]}x`,`${aux[0] - 1}y${aux[1]}x` )
+                    verificadorPosiciones.push(`${aux[0] - 1}y${parseInt(aux[1]) - 1}x`, `${aux[0]}y${parseInt(aux[1]) - 1}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) - 1}x`)
+                    verificadorPosiciones.push(`${parseInt(aux[0]) - 1}y${parseInt(aux[1]) + 4}x`, `${aux[0]}y${parseInt(aux[1]) + 4}x`, `${parseInt(aux[0]) + 1}y${parseInt(aux[1]) + 4}x`)
+                }
+            }
+        } */
     }
     if (barcosJugador.length === 10) {
         tableroEnemigo.style.display = ""
@@ -188,9 +279,25 @@ const pintaJugador = (e) => {
     }
 };
 /* recarga la pagina */
-const cargarPagina = () =>{
+const cargarPagina = () => {
     location.reload()
+    modoJuego = ""
 };
+/* modo lanchas */
+const jugarConLanchas = () => {
+    instruccionesLanchas.style.display = "block"
+    opcionesJuego.style.display = "none"
+    tableroJugador.style.display = ""
+    modoJuego = "lanchas"
+};
+/* modo tradicional */
+/* const jugarConTodo = () => {
+    instruccionesBarcos.style.display = ""
+    opcionesJuego.style.display = "none"
+    tableroJugador.style.display = ""
+    detalleBarcos.style.display = ""
+    modoJuego = "tradicional"
+}; */
 
 
 
@@ -213,3 +320,5 @@ span.addEventListener("click", cerrarModal);
 input.addEventListener("change", capturaValor);
 disparaMisil.addEventListener("click", dispararAlBarco)
 jugarDeNuevo.addEventListener("click", cargarPagina)
+opcionLanchas.addEventListener("click", jugarConLanchas)
+opcionFullBarcos.addEventListener("click", jugarConTodo)
